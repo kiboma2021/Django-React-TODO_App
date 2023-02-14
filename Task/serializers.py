1cb74a3f26cef5949bde0c16b1d00c7096d6735e
kiboma <omayioben@gmail.com>
2023-02-14 08:13:44 +0300

    Add view

---
+++ Task/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+from .models import Item
+
+def TaskSerializer(serializers.modelSerializer):
+    sss
+    
--- Task/views.py
+++ Task/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Task
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 
 # Create your views here.
+
+@api_view(['GET', 'POST'])
+def MyTasksView(request):
+    try:
+        get_task=Task.objects.all()
+    except Task.DoesNotExist:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    if request.method == 'GET':
+        serializer = TaskSerializer(get_task, many=True)
+        return Response(status=status.HTTP_200_OK
+    
